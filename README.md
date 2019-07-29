d86d8a8ace17c5ee9e5da2db40cde460905fa8fe
Sjur Millidahl <sjur.millidahl@finn.no>
2019-07-29 13:12:30 +0200

    Initial commit 0.0.1

---
+++ README.md
@@ -0,0 +1,11 @@
+# ormolu-vscode
+VS Code extension for Haskell code formatting based on [ormolu tool](https://github.com/tweag/ormolu)
+
+## Usage
+
+1. Install ormolu `git clone https://github.com/tweag/ormolu && cd ormolu && stack install`
+2. Use VS Code's built-in format or format selection commands.
+
+## Credits
+
+Code and configuration for the Extension is based on https://github.com/sergey-kintsel/hfmt-vscode by Sergey Kintsel
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+const haskellLangId = 'haskell';
+
+export function activate(context: vscode.ExtensionContext) {
+  vscode.languages.registerDocumentRangeFormattingEditProvider(haskellLangId, {
+    provideDocumentRangeFormattingEdits(document, range, options, token) {
+      const text = document.getText(range);
+      try {
+        const ormolu = cp.execSync('ormolu', { input: text });
+        const formattedText = ormolu.toString();
+        return [vscode.TextEdit.replace(range, formattedText)];
+      } catch (e) {
+        vscode.window.showErrorMessage("ormolu failed to format the code. " + e.stdout.toString());
+        console.log(e.stdout.toString());
+      }
+    }
+  });
+}
