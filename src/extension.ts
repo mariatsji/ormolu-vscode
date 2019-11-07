84571e6b3d98050b131a2e4bfb3806b81bee4ea2
Henri Maurer <hmaurer@users.noreply.github.com>
2019-11-07 10:54:19 +0000

    Make ormolu executable path configuration (#10)
    * Make ormolu executable path configuration
    
    * Bump version number to 0.0.5 and change configuration name

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,21 @@
-import * as vscode from 'vscode';
-import * as cp from 'child_process';
+import * as vscode from "vscode";
+import * as cp from "child_process";
 
-const haskellLangId = 'haskell';
+const haskellLangId = "haskell";
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDocumentRangeFormattingEditProvider(haskellLangId, {
     provideDocumentRangeFormattingEdits(document, range, options, token) {
       const text = document.getText(range);
       try {
-        const ormolu = cp.execSync('ormolu', { input: text });
+        const config = vscode.workspace.getConfiguration("ormolu");
+        const ormolu = cp.execSync(config.path, { input: text });
         const formattedText = ormolu.toString();
         return [vscode.TextEdit.replace(range, formattedText)];
       } catch (e) {
-        vscode.window.showErrorMessage("ormolu failed to format the code. " + e.stderr.toString());
+        vscode.window.showErrorMessage(
+          "ormolu failed to format the code. " + e.stderr.toString()
+        );
         console.log(e.stdout.toString());
       }
     }
