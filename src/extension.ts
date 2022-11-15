13991ecc2abe366292060a85af242bdad695985a
Logan Tibbetts <Lcubed37@gmail.com>
2022-11-15 13:34:49 -0600

    Added args support

--- src/extension.ts
+++ src/extension.ts
@@ -4,21 +4,30 @@ import * as cp from "child_process";
 const haskellLangId = "haskell";
 
 export function activate(context: vscode.ExtensionContext) {
+  // Create output channel
+  let log = vscode.window.createOutputChannel("Ormolu VSCode");
+
   vscode.languages.registerDocumentRangeFormattingEditProvider(haskellLangId, {
     provideDocumentRangeFormattingEdits(document, range, options, token) {
       const text = document.getText(range);
       try {
         const config = vscode.workspace.getConfiguration("ormolu");
-        const ormolu = cp.execSync(config.path, { input: text, cwd: vscode.workspace.getWorkspaceFolder(document.uri).uri.path });
+        const args = config.args.join(" ");
+        var cmd = config.path
+        if (config.args.length > 0) {
+          cmd = config.path + " " + args;
+        }
+        log.appendLine("Calling: " + cmd);
+        const ormolu = cp.execSync(cmd, { input: text, cwd: vscode.workspace.getWorkspaceFolder(document.uri).uri.path });
         const formattedText = ormolu.toString();
         return [vscode.TextEdit.replace(range, formattedText)];
       } catch (e) {
+        log.appendLine(e.stderr.toString());
         if (vscode.workspace.getConfiguration('ormolu').get('notifyOnParseError')) {
           vscode.window.showErrorMessage(
             " ormolu failed to format the code. " + e.stderr.toString()
           );
         }
-        console.log(e.stdout.toString());
       }
     }
   });
